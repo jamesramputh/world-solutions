3fff9788a06905d652436e0a969a0373576c567f
jamesramputh <james.ramputh@gmail.com>
2015-08-14 18:20:22 -0400

    Ws-Master: setup layout file.

---
+++ Framework/resources/views/layout.blade.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <title>World Solutions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/lightbox.min.js"></script>
+    <link href="css/lightbox.css" rel="stylesheet" />
+    <link href="css/bootstrap.min.css"  type= "text/css" rel= "stylesheet">
+    <link href="css/styles/main.css" type= "text/css" rel= "stylesheet">
+    <link href="css/animate.css" type= "text/css" rel= "stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+    <!--jQuery and bootstrap library -->
+    <script src="js/custom.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!--JQuery UI -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+    <!-- wow.js library -->
+    <script src="js/wow.js"></script>
+
+</head>
+
+<body>
+    <header>
+        <nav id="navbar" class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">World Solutions</a>
+
+                <div class="collapse navbar-collapse navHeaderCollapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a id="top_anchor" title="Home Jump" href="#home_section">Home</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="clear"></div>
+    </header>
+
+    @yield("content")
+
+</body>
+
+
+
+<div id="footer">
+    <div class="container">
+        <div class="footer-content">&copy <?php echo date('Y'); ?> World Solutions</div>
+    </div>
+</div>
+
+
+</html>
